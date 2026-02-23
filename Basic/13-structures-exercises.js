9730d245e79d26073c9750f6cd6bd8411ddb5633
dieswae <diegoo.rz.dev@gmail.com>
2026-02-22 23:12:58 -0500

    ejercicios de estructuras y loops

--- Basic/13-structures-exercises.js
+++ Basic/13-structures-exercises.js
@@ -5,20 +5,67 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=11451
 
 // 1. Crea un array que almacene cinco animales
 
+let animals = ["fox", "serpent", "dragon", "unicorn", "lion"]
+
+console.log(animals);
+
+
 // 2. Añade dos más. Uno al principio y otro al final
+
+animals.push("shark")
+console.log(animals)
+animals.unshift("cat")
+console.log(animals);
 
 // 3. Elimina el que se encuentra en tercera posición
 
+animals.splice(2, 1)
+console.log(animals)
+
 // 4. Crea un set que almacene cinco libros
+
+let myBooks = new Set (["cumbres borrascosas", "pinocho", "mobydick", "got", "js eloquent"])
 
 // 5. Añade dos más. Uno de ellos repetido
 
+myBooks.add("agatha christie")
+myBooks.add("got")
+console.log(myBooks)
+
 // 6. Elimina uno concreto a tu elección
+
+myBooks.delete("agatha christie")
+console.log(myBooks);
+
 
 // 7. Crea un mapa que asocie el número del mes a su nombre
 
+let months = new Map([
+  [1, "Enero"],
+  [2, "Febrero"],
+  [3, "Marzo"],
+  [4, "Abril"],
+  [5, "Mayo"]
+])
+
 // 8. Comprueba si el mes número 5 existe en el map e imprime su valor
+
+console.log(months.has(5))
+console.log(months.get(5))
 
 // 9. Añade al mapa una clave con un array que almacene los meses de verano
 
+months.set("Summer", ["Febrero, Marzo, Abril"])
+console.log(months)
+
 // 10. Crea un Array, transfórmalo a un Set y almacénalo en un Map
+
+let myArray = ["mariale", "diego", "ginny", "missy"]
+
+let mySet = new Set(myArray)
+
+let myMap = new Map([["family", mySet]])
+
+console.log(myArray)
+console.log(mySet)
+console.log(myMap)
--- Basic/15-loops-exercises.js
+++ Basic/15-loops-exercises.js
@@ -6,21 +6,100 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=12732
 // NOTA: Explora diferentes sintaxis de bucles para resolver los ejercicios
 
 // 1. Crea un bucle que imprima los números del 1 al 20
+let a = 1
+while (a <= 20) {
+  console.log(a)
+  a++
+}
 
 // 2. Crea un bucle que sume todos los números del 1 al 100 y muestre el resultado
 
+let sum = 0
+for (let i = 0; i <= 100; i++) {
+  sum += i
+}
+console.log(sum)
+
 // 3. Crea un bucle que imprima todos los números pares entre 1 y 50
 
+for (let i = 1; i <= 50; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+}
+
 // 4. Dado un array de nombres, usa un bucle para imprimir cada nombre en la consola
+let names = ["mariale", "diego", "ginny", "missy"]
+
+for (const values of names) {
+  console.log(values)  
+}
 
 // 5. Escribe un bucle que cuente el número de vocales en una cadena de texto
 
+let myString = "camaron que se duerme"
+let count = 0
+
+myString.toLowerCase()
+for (const letter of myString) {
+  if ("aeiou".includes(letter)) {
+    count ++
+  }
+}
+console.log(count)
+
 // 6. Dado un array de números, usa un bucle para multiplicar todos los números y mostrar el producto
+
+let nums = [4, 6, 4, 10]
+let multipler = 1
+for (const value of nums) {
+  multipler *= value 
+}
+console.log(multipler)
 
 // 7. Escribe un bucle que imprima la tabla de multiplicar del 5
 
+
+for (let i = 1; i <= 10; i++) {
+  console.log(`5 x ${i} = ${5 * i}`)
+}
+
 // 8. Usa un bucle para invertir una cadena de texto
+
+let username = "diklaus"
+let invert = ""
+
+for(let i = username.length - 1; i >= 0; i--) {
+  invert += username[i]
+}
+
+console.log(invert)
 
 // 9. Usa un bucle para generar los primeros 10 números de la secuencia de Fibonacci
 
+let c = 0
+let d = 1
+
+console.log(c)
+console.log(d)
+
+for(let i = 0; i < 8; i++) {
+  let siguiente = c + d
+  console.log(siguiente)
+  c = d
+  d = siguiente
+}
+
 // 10. Dado un array de números, usa un bucle para crear un nuevo array que contenga solo los números mayores a 10
+
+let array10 = [4, 14, 100, 24, 6, 2, 20]
+
+let mayor = []
+
+for (let num of array10) {
+  if (num > 10) {
+    mayor.push(num)
+  }
+}
+
+console.log(mayor)
